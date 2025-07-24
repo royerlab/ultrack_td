8afc6c4bf69d44a90defcee6a1aaa16ece00017b
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2025-07-24 10:32:41 -0400

    removing unecessary files

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use pyo3::prelude::*;
 
 mod connected_components;
 mod hierarchical_segmentation;
-mod mask;
 
 #[pymodule]
 fn _rustlib<'py>(_py: Python<'py>, m: &Bound<'py, PyModule>) -> PyResult<()> {
@@ -22,7 +21,5 @@ fn _rustlib<'py>(_py: Python<'py>, m: &Bound<'py, PyModule>) -> PyResult<()> {
         hierarchical_segmentation::hierarchical_segmentation,
         m
     )?)?;
-    m.add_function(wrap_pyfunction!(mask::create_component_mask_2d, m)?)?;
-    m.add_function(wrap_pyfunction!(mask::create_component_mask_3d, m)?)?;
     Ok(())
 }
